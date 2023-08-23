25b59ee9a6b142068e54ef3afc6c53f3dad174d4
jtjlehi <jtjlehi@gmail.com>
2023-08-23 16:34:06 -0600

    added new keybindings

--- lua/keys.lua
+++ lua/keys.lua
@@ -13,6 +13,15 @@ vim.keymap.set('i', 'jj', '<Esc>')
 -------------------------------------------------------------------------------
 
 -- Toggle nvim-tree
-vim.keymap.set('n', L..'n', function() vim.cmd('NvimTreeToggl') end)
+vim.keymap.set('n', L .. 'n', function() vim.cmd('NvimTreeToggl') end)
 -- Clear Search
-vim.keymap.set('n', L..'cs', function() vim.cmd('nohlsearch') end)
+vim.keymap.set('n', L .. 'cs', function() vim.cmd('nohlsearch') end)
+-- Open terminal
+vim.keymap.set('n', L .. 't', function() vim.cmd('term') end)
+-- show errors
+vim.keymap.set('n', L .. 'e', vim.diagnostic.open_float)
+-- Toggle Settings
+vim.keymap.set('n', L .. L, function()
+    vim.cmd('tabnew ~/.config/nvim')
+    vim.cmd('cd ~/.config/nvim')
+end)
