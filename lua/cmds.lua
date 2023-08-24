7ab848f548c91ec09ec2076ed7aef255ce27f44b
jtjlehi <jtjlehi@gmail.com>
2023-08-23 21:28:29 -0600

    updated open settings key to close if already open

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -6,6 +6,9 @@ vim.api.nvim_create_user_command('OpenSettings', function(opts)
     local config = "edit ~/.config/nvim/"
 
     local settings_file = opts.fargs[1]
-    if not settings_file then vim.cmd(config.."init.lua")
-    else vim.cmd(config.."lua/"..settings_file..".lua") end
-end, {nargs = '?'})
+    if not settings_file then
+        vim.cmd(config .. "init.lua")
+    else
+        vim.cmd(config .. "lua/" .. settings_file .. ".lua")
+    end
+end, { nargs = '?' })
--- lua/keys.lua
+++ lua/keys.lua
@@ -22,6 +22,12 @@ vim.keymap.set('n', L .. 't', function() vim.cmd('term') end)
 vim.keymap.set('n', L .. 'e', vim.diagnostic.open_float)
 -- Toggle Settings
 vim.keymap.set('n', L .. L, function()
-    vim.cmd('tabnew ~/.config/nvim')
-    vim.cmd('cd ~/.config/nvim')
+    local settingsPath = '.config/nvim'
+    local cwdPath = vim.fn.getcwd():sub(- #settingsPath)
+    if cwdPath == settingsPath then -- close settings
+        vim.cmd('tabclose')
+    else                            -- open settings
+        vim.cmd('tabnew ~/' .. settingsPath)
+        vim.cmd('cd ~/' .. settingsPath)
+    end
 end)
