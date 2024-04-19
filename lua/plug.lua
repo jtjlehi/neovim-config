2af5cddb87bbb9ddb81741706b22312a26bfc548
jtjlehi <jtjlehi@gmail.com>
2024-04-18 18:47:18 -0600

    Switched to `lazy.nvim` from packer

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,93 +1,100 @@
--- [[ plug.lua ]]
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-    -- add packer to remove the warning
-    use 'wbthomason/packer.nvim'
+require("lazy").setup({
     -- tree
-    use {
+    {
         'kyazdani42/nvim-tree.lua',
-        requires = 'kyazdani42/nvim-web-devicons'
-    }
-    use 'mhinz/vim-startify' -- start screen
+        dependencies = 'kyazdani42/nvim-web-devicons'
+    },
+    'mhinz/vim-startify', -- start screen
     -- [[ Tree Sitter ]]
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = ":TSUpdate"
-    }
-    use { "HiPhish/rainbow-delimiters.nvim" }
+        build = ":TSUpdate"
+    },
+    { "HiPhish/rainbow-delimiters.nvim" },
     -- [[ Color Theme/Stylying ]]
-    use "ellisonleao/gruvbox.nvim"
+    "ellisonleao/gruvbox.nvim",
     -- statusline
-    use {
+    {
         'nvim-lualine/lualine.nvim',
-        requires = 'kyazdani42/nvim-web-devicons'
-    }
-    use { "lukas-reineke/indent-blankline.nvim", main = "ibl" }
+        dependencies = 'kyazdani42/nvim-web-devicons'
+    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
     -- [[ Git ]]
-    use 'tpope/vim-fugitive'
-    use 'airblade/vim-gitgutter'
+    'tpope/vim-fugitive',
+    'airblade/vim-gitgutter',
     -- [[ LSP ]]
-    use "lukas-reineke/lsp-format.nvim"
-    use 'neovim/nvim-lspconfig'
-    use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            {
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    }
-    use 'simrat39/rust-tools.nvim'
-    use { 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } }
-    use {
+    { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+    "lukas-reineke/lsp-format.nvim",
+    'neovim/nvim-lspconfig',
+    { 'neovim/nvim-lspconfig' },
+    {
+        'williamboman/mason.nvim',
+        build = function()
+            pcall(vim.cmd, 'MasonUpdate')
+        end,
+    },
+    { 'williamboman/mason-lspconfig.nvim' },
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'L3MON4D3/LuaSnip' },
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = 'kevinhwang91/promise-async'
+    },
+    'simrat39/rust-tools.nvim',
+    { 'scalameta/nvim-metals', dependencies = { "nvim-lua/plenary.nvim" } },
+    {
         'mrcjkb/haskell-tools.nvim',
         ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
-    }
+    },
     -- [[ Autocompletion ]]
-    use {
+    {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
-    }
-    use {
+    },
+    {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end
-    }
+    },
     -- [[Toggle Term]]
-    use {
+    {
         "akinsho/toggleterm.nvim",
         config = function()
             require("toggleterm").setup()
         end
-    }
+    },
     -- [[Telescope]]
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        requires = {
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = {
             { 'nvim-lua/plenary.nvim' },
             { "nvim-telescope/telescope-live-grep-args.nvim" },
         },
         config = function()
             require("telescope").load_extension("live_grep_args")
         end
-    }
+    },
     -- [[Markdown]]
-    use({
+    {
         "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
-end)
+        build = function() vim.fn["mkdp#util#install"]() end,
+    }
+})
